b9d51e444e8ce78a004eada0163f828794d5a9c6
Nina <labferment10@gmail.com>
2024-11-29 16:30:20 +0100

    fixed cors issue, socket initial setup

--- backend/index.js
+++ backend/index.js
@@ -3,20 +3,31 @@ const socketio = require("socket.io");
 const http = require("http");
 const PORT = process.env.PORT || 5000;
 const router = require("./router");
+const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+
+// middleware functions
+// configuring CORS for express
+app.use(cors()); // CORS should be called before router
+app.use(router);
+
+// CORS for Socket.io, because without this the app crashes
+const io = socketio(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
 
 io.on("connection", (socket) => {
-  console.log("We have a new connection!", socket);
+  console.log("We have a new connection!");
 
   socket.on("disconnect", () => {
     console.log("Connection is disconnected!!!");
   });
 });
 
-app.use(router);
 server.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
--- frontend/src/components/chat.tsx
+++ frontend/src/components/chat.tsx
@@ -1,12 +1,26 @@
 import queryString from "query-string";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router";
+import { io, Socket } from "socket.io-client";
+
+let socket: Socket;
+
 export const Chat = () => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
   const location = useLocation();
+  const ENDPOINT = "localhost:5000";
+
   useEffect(() => {
-    const data = queryString.parse(location.search);
-    console.log(data);
-  }, [location]);
+    const { name, room } = queryString.parse(location.search);
+    setName(name as string);
+    setRoom(room as string);
+
+    // Connect to the socket
+    socket = io(ENDPOINT);
+    console.log(socket);
+  }, [location.search, ENDPOINT]);
+
   return (
     <div>
       <h2>Chat</h2>
